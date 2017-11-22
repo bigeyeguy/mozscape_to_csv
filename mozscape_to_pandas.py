2a3807795469c4e1f17fb83874dc2ad8c5469839
bigeyeguy <stuartvictorjones@gmail.com>
2017-11-22 22:04:45 +0000

    First commit

---
+++ mozscape_to_pandas.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+from mozscape import Mozscape
+import argparse
+import csv
+import sys
+import pandas as pd
+
+client = Mozscape(
+    'my-access-id',
+    'my-secret-key')
+
+# The links API has more columns to specify, as well as sort, scope, etc.
+links = client.links(
+    'ENTER URL', scope='page_to_page', sort='page_authority',
+    filters=['internal'], targetCols=Mozscape.UMCols.url)
+	
+
+## PUT ABOVE INTO A PANDAS DATAFRAME ##
+#df = pd.DataFrame(authorities.items())
+df = pd.DataFrame(links)
+df = df.rename(columns={
+	'upa': 'Page Authority', 
+	'uu': 'URL'})
+
+print(df)
+df.to_excel('mozLinks.xls', index=False)
+# use xlsx to stop limiting of row export - check csv module
+
+
+anchorTermResults = client.anchorText(
+		'www.furniture-work.co.uk/office_chairs',
+		 cols=Mozscape.ATCols.freeCols2)
+
+df2 = pd.DataFrame(anchorTermResults)
+df2 = df2.rename(columns={
+	'apuemp': 'External Mozrank passed', 
+	'apuimp': 'Internal Mozrank Passed', 
+	'apuiu': 'Internal Pages Linking', 
+	'aput': 'Term'})
+print(df2)
+
+df2.to_excel('anchors.xls', index=False)
+# print(df)
+
+
+"""
+default metrics when doing print(mozMetrics)
+
+uu == canonical form of URL
+ut == meta title
+us == http status code
+upa == page authority
+ueid == equity of external links?
+ulc == time last crawled returned in epoch format (epochconverter.com/)
+umrp == 10 point score of Mozrank
+umrr == Mozrank raw score
+fmrp == subdomain mozRank 10 point score
+fmrr == above raw score
+pda == domain authority
+
+default metrics when doing print(anchorText)
+
+apuef ==
+apuemp == external mozrank passed - as for apuimp, but external
+apuep ==
+apueu ==
+apuf ==
+apuif ==
+apuimp == internal mozrank passed - The amount of MozRank passed over all internal links
+								    with this term or phrase (on a normalized 10-point 
+								    scale)
+
+apuiu == internal pages linking - The number of internal pages on a root domain 
+								  linking to the target URL with this term or phrase
+aput == term
+"""
+
